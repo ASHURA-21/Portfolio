20c676afe0b2b65e6caa3f2a563dccbce8bb4600
ASHURA-21 <157248274+ASHURA-21@users.noreply.github.com>
2025-03-04 17:28:55 +0100

    portfolio commit

---
+++ form.js
@@ -0,0 +1,33 @@
+function save(){
+    let first = document.getElementById('validationDefault01').value;
+    let second = document.getElementById('validationDefault02').value;
+    let third = document.getElementById('exampleFormControlInput1').value;
+    let fourth = document.getElementById('floatingSelect').value;
+    let fifth = document.getElementById('exampleFormControlTextarea1').value;
+    let sixth = document.getElementById('validationDefault03').value;
+    let seventh = document.getElementById('inputZip').value;
+    let eight = document.getElementById('formGroupExampleInput').value;
+    let nine = document.getElementById('invalidCheck2').value;
+    
+    let data = {
+      first_name: first,
+      last_name: second,
+      phone_number: third,
+      service: fourth,
+      job_description: fifth,
+      city: sixth,
+      zip: seventh,
+      date: eight,
+      terms: nine
+    }
+
+    
+    let data_string = JSON.stringify(data);
+    
+    let file = new Blob([data_string],{type:"text/plain"});
+    let anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(file);
+    anchor.download = "save.txt";
+    anchor.click();
+    console.log('your data has been saved')
+  }
